d1505fbc7e6b8965a94e3f65ca945cb3feee62f6
Егор <42974925+MrLaPka@users.noreply.github.com>
2018-09-21 00:33:57 +0300

    3task

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
-class Sorter {
-  constructor() {
-    // your implementation
+class Sorter { 
+  constructor() {  
+  this.length = 0; 
+  this.compareFunction = null; 
+  this.array = []; 
+  } 
+  
+  add(element) { 
+  this.array.push(element); 
+  this.length+=1; 
+  } 
+  
+  at(index) { 
+  return this.array[index]; 
+  } 
+  get length() { 
+  return this._length; 
+  } 
+  
+  set length(value) { 
+  this._length = value; 
+  } 
+  
+  toArray() { 
+  return this.array; 
+  } 
+  sort(indices) { 
+  var array2 = []; 
+  for(var i = 0; i<indices.length;i++){ 
+  array2.push(this.array[indices[i]]); 
+  } 
+  indices = indices.sort((a,b)=>a-b); 
+  if(this.compareFunction === null){ 
+  array2 = array2.sort((a,b)=>a-b); 
+  }else{ 
+  array2 = array2.sort(this.compareFunction); 
+  } 
+  for(var i=0;i<array2.length;i++){ 
+  this.array[indices[i]] = array2[i]; 
+  } 
+  } 
+  // we initial our compare function 
+  setComparator(compareFunction) { 
+  this.compareFunction = compareFunction; 
+  } 
   }
-
-  add(element) {
-    // your implementation
-  }
-
-  at(index) {
-    // your implementation
-  }
-
-  get length() {
-    // your implementation
-  }
-
-  toArray() {
-    // your implementation
-  }
-
-  sort(indices) {
-    // your implementation
-  }
-
-  setComparator(compareFunction) {
-    // your implementation
-  }
-}
 
 module.exports = Sorter;
